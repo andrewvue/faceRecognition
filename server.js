446c56414605a727c94f76f4d7aa976a7f35b910
andrew <vuexandrew@gmail.com>
2020-07-30 16:06:05 -0500

    updating response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors());
 
 
 app.get('/', (req, res)=> {
-    res.send('it is working');
+    res.send('it is working')
 })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
